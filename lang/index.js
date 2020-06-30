ebc4c00a26c4637f089a273e452c2753fc43cbff
VINXIS <oykxf2@gmail.com>
2020-06-30 17:26:16 -0600

    FIX STUFF

--- lang/index.js
+++ lang/index.js
@@ -1,13 +1,7 @@
-import cn from './cn';
-import de from './de';
-import en from './en';
-import fr from './fr';
-import id from './id';
-import kr from './kr';
-import nl from './nl';
-import ru from './ru';
-import tw from './tw';
+import en from "./en";
+import id from "./id";
+import kr from "./kr";
 
 export default {
-    cn, de, en, fr, id, kr, nl, ru, tw
-}
+    en, id, kr,
+};
