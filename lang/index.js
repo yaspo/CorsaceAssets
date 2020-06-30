2e32c31c6472470850e947532c57a430c1b45f7b
yaspo <yasporubenvl@gmail.com>
2020-07-01 00:14:31 +0200

    Language Switcher (#17)
    * unignored lang/index.js so it can be public
    
    * Added index.js file to allow importing of all languages in other files

---
+++ lang/index.js
@@ -0,0 +1,13 @@
+import cn from './cn';
+import de from './de';
+import en from './en';
+import fr from './fr';
+import id from './id';
+import kr from './kr';
+import nl from './nl';
+import ru from './ru';
+import tw from './tw';
+
+export default {
+    cn, de, en, fr, id, kr, nl, ru, tw
+}
